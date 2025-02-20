ab5664cb7355a0f844ce4943cba1d98b4ace5b20
Haftom <haftish.tsegay@gmail.com>
2025-02-20 03:31:56 -0500

    completed caption logic

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -1,45 +1,40 @@
-/** @jsxImportSource @emotion/react */
 import React, { useEffect, useState, useRef } from 'react';
 import './carousel.css';
 import ArrowRight from '../assets/arrow-right.svg?react';
 import ArrowLeft from '../assets/arrow-left.svg?react';
-import { css } from '@emotion/react';
 
 const Carousel = ({ images }) => {
 
     const [currentIndex, setCurrentIndex] = useState(0);
-    const imageref = useRef(null);
+    const [captionState, setCaptionState] = useState(0);
+    const imageRef = useRef(null);
+    const textRef = useRef(null);
+    const seeMoreButtonRef = useRef(null);
 
-    const captionStyle = css`
-        ::after {
-            content: "";
-            position: absolute;
-            bottom: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-            background-color: rgba(0, 0, 0, 0.5);
-            color: white;
-            padding: 1rem;
-            font-size: 1.5rem;
-            text-align: center;
+    useEffect(() => {
+        if (textRef.current.scrollWidth > textRef.current.clientWidth){
+            setCaptionState(1)
         }
-    `;
-
+        else setCaptionState(0)
+        return ()=>{console.log(captionState)}
+    }, [images, currentIndex])
+    const handleSeeMore = () => {
+        if (captionState === 1) setCaptionState(2)
+        else setCaptionState(1)
+    }
     const handleImageLoaded = () => {
-        imageref.current.classList.remove('slide-in');
-
+        imageRef.current.classList.remove('slide-in');
     }
 
     const nextSlide = () => {
-        imageref.current.classList.add('slide-in');
+        imageRef.current.classList.add('slide-in');
         setTimeout(()=>{
             setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
         }, 300)
     };
 
     const prevSlide = () => {
-        imageref.current.classList.add('slide-in');
+        imageRef.current.classList.add('slide-in');
         setTimeout(()=>{
             setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
         }, 300)
@@ -55,8 +50,11 @@ const Carousel = ({ images }) => {
         <div className="carousel">
             <button onClick={prevSlide} className="carousel-button prev"><ArrowLeft /></button>
             <div className='slide'>
-                <img css={captionStyle} onLoad={handleImageLoaded} ref={imageref} src={images[currentIndex].url} alt="carousel" className="carousel-image slide-in" />
-                <div className='caption'>{images[currentIndex].caption}</div>
+                <img onLoad={handleImageLoaded} ref={imageRef} src={images[currentIndex].url} alt="carousel" className="carousel-image slide-in" />
+                <div className='caption'>
+                    <div ref={textRef} className={captionState !== 2 ? 'text-elipsis': ''}>{images[currentIndex].caption}</div>
+                    <button onClick={handleSeeMore} ref={seeMoreButtonRef} className={captionState === 0 ? 'hide': ''}>{captionState==1?"more":"less"}</button>
+                </div>
             </div>
             <button onClick={nextSlide} className="carousel-button next"><ArrowRight /></button>
             <div className='carousel-dots'>
